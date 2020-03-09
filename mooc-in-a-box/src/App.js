7e458678aa237e956be8e228bf3d389a9b43e996
Jessi Beck <jessi@jessis-mbp.lan>
2020-03-08 21:55:57 -0700

    moving logic for fetching users up a level, converting two components to ES6 classes

--- mooc-in-a-box/src/App.js
+++ mooc-in-a-box/src/App.js
@@ -13,11 +13,19 @@ import CourseLibrary from './components/CourseLibrary/CourseLibrary';
 import CourseOverview from './components/CourseOverview/CourseOverview';
 import firebaseInstance from './config/firebase';
 import Home from './components/Home/Home';
+import { render } from '@testing-library/react';
 
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {courses: []};
+    this.fetchCourses = this.fetchCourses.bind(this);
+    this.render = this.render.bind(this);
+    this.fetchCourses();
+  }
 
-  useEffect(() => {
+  useEffect() {
     // Update the document title using the browser API
     console.log("Helloooo world!");
     const db  = firebaseInstance.firestore();
@@ -27,41 +35,61 @@ function App() {
           console.log(doc.data());
         });
       })
-  });
+    }
+    
+    async fetchCourses() {
+      const db  = firebaseInstance.firestore();
+      let courses = [];
+      await db.collection("Course").get().then( (queryResults) => {
+        queryResults.forEach((doc) => {
+          const course = doc.data();
+          course.owner.get().then(user => {
+            course.owner = user.data();
+          })
+          course.chapter.lessons.get().then(lesson => {
+            course.chapter.lessons = lesson.data();
+          })
+          courses.push(course);
+          this.setState({courses: courses});
+        });
+      });
+      console.log(courses)
+    }
 
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className="menu-button" color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Button color="inherit" className="menu-button" component={RouterLink} to="/">MOOC-In-A-Box</Button>
+              <Button color="inherit" className="menu-button" component={RouterLink} to="/courseOverview">Course Overview</Button>
+              <Button color="inherit" className="menu-button" component={RouterLink} to="/courseLibrary">Course Library</Button>
+            </Toolbar>
+          </AppBar>
+          </nav>
 
-  return (
-    <Router>
-      <div>
-        <nav>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className="menu-button" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Button color="inherit" className="menu-button" component={RouterLink} to="/">MOOC-In-A-Box</Button>
-            <Button color="inherit" className="menu-button" component={RouterLink} to="/courseOverview">Course Overview</Button>
-            <Button color="inherit" className="menu-button" component={RouterLink} to="/courseLibrary">Course Library</Button>
-          </Toolbar>
-        </AppBar>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/courseLibrary">
-            <CourseLibrary />
-          </Route>
-          <Route path="/courseOverview">
-            <CourseOverview />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/courseLibrary">
+              <CourseLibrary courses={this.state.courses}/>
+            </Route>
+            <Route path="/courseOverview">
+              <CourseOverview />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+};
 
 export default App;
--- mooc-in-a-box/src/components/CourseLibrary/CourseLibrary.js
+++ mooc-in-a-box/src/components/CourseLibrary/CourseLibrary.js
@@ -8,35 +8,11 @@ import { render } from '@testing-library/react';
 class CourseLibrary extends React.Component {
   constructor(props) {
     super(props);
-    this.getCourses();
   }
   
-  async getCourses() {
-    const db  = firebaseInstance.firestore();
-    this.courses = [];
-    await db.collection("Course").get().then( (queryResults) => {
-      queryResults.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data()}`);
-        // console.log(doc.data());
-        const course = doc.data();
-        course.owner.get().then(user => {
-          course.owner = user.data();
-          // console.log(user.data())
-        })
-        course.chapter.lessons.get().then(lesson => {
-          course.chapter.lessons = lesson.data();
-          // console.log(lesson.data())
-        })
-        // console.log(course);
-        this.courses.push(course);
-        this.setState({data: this.courses});
-      });
-    });
-    console.log(this.courses)
-  }
 
   render() {
-    let listItems = this.courses.map(function(item) {
+    let listItems = this.props.courses.map(function(item) {
       return (
         <div className="CurrentCourse">
           <CourseCard course={item}
