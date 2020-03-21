fcc7143f70c053a9aa31a57f8057e447b303db6a
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2020-03-21 11:34:07 -0400

    Updating to include a search bar and mini app bar on course library

--- mooc-in-a-box/src/components/CourseLibrary/CourseLibrary.js
+++ mooc-in-a-box/src/components/CourseLibrary/CourseLibrary.js
@@ -3,16 +3,59 @@ import './CourseLibrary.css';
 import {Button} from '@material-ui/core';
 import CourseCard from '../CourseCard/CourseCard';
 import { render } from '@testing-library/react';
+import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
+
+
+
+
+
+
+
+import {AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+
 
 import * as FirebaseService from '../../service/firebase.service'
+
+
+function SearchBarComponent(props) {
+  console.log(props);
+  return(
+    <div className="search-bar">
+        <TextField value={props.searchValue} onChange={props.onChange} id="outlined-search" type="search" variant="outlined"  color="white"/>
+        <IconButton className="menu-button" onClick={props.onSearchClicked}>
+          <SearchIcon className="search-bar-icon" />
+        </IconButton>
+    </div>
+  )
+}
+
 
 class CourseLibrary extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSearchClicked = this.handleSearchClicked.bind(this);
+    this.state = {
+      searchValue: ""
+    }
+  }
+
+  handleSearchChange(e){
+    console.log("Key Entered!")
+    this.setState({
+      searchValue: e.target.value
+    })
+  }
+
+  handleSearchClicked(e){
+    console.log("Search button clicked!")
+    console.log(this.state.searchValue);
   }
 
   
   render() {
+
     let listItems = this.props.courses.map(function(item) {
       return (
         <div className="CurrentCourse">
@@ -22,7 +65,23 @@ class CourseLibrary extends React.Component {
       );
     });
     
-    return (listItems);
+    return (
+      <div class="course-library">
+        <AppBar position="static" className="course-library-app-bar">
+            <Toolbar>
+              <Button color="inherit" className="menu-button">Sort By</Button>
+              <Button color="inherit" className="menu-button">Topic</Button>
+              <Button color="inherit" className="menu-button">Organization</Button>
+              <Button color="inherit" className="menu-button">My Courses</Button>
+              <SearchBarComponent searchValue={this.state.searchValue} onChange={this.handleSearchChange} onSearchClicked={this.handleSearchClicked}></SearchBarComponent>
+              </Toolbar>
+          </AppBar>
+          <div className="course-list">
+            {listItems}
+          </div>
+      </div>
+
+      );
   }
 }
 
