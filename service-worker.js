3628da196f9acb328fd400c9f9a74d2e43083822
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2020-04-22 19:37:23 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66eb84252fac1f85fc1e18073d93bc3c.js"
+  "/precache-manifest.934dbc847bd02bd097abfe3c1589ec65.js"
 );
 
 self.addEventListener('message', (event) => {
